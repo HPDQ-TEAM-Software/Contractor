aa9de7bed8c28a38a5ab1f47085c3b250bc56d06
ITHPDQ <anhtuan716@gmail.com>
2021-06-04 16:29:42 +0700

    login

--- QLNHATHAU/Controllers/LoginController.cs
+++ QLNHATHAU/Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using QLNHATHAU.Models;
 
 namespace QLNHATHAU.Controllers
 {
@@ -11,6 +13,33 @@ namespace QLNHATHAU.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Index(UserLogin u)
+        {
+            //UsersEntities usersEntities = new UsersEntities();
+            //int? userId = usersEntities.ValidateUser(user.Username, user.Password).FirstOrDefault();
+
+            //string message = string.Empty;
+            //switch (userId.Value)
+            //{
+            //    case -1:
+            //        message = "Username and/or password is incorrect.";
+            //        break;
+            //    case -2:
+            //        message = "Account has not been activated.";
+            //        break;
+            //    default:
+            //        FormsAuthentication.SetAuthCookie(user.Username, user.RememberMe);
+            //        return RedirectToAction("Profile");
+            //}
+
+            //ViewBag.Message = message;
+
+            //string idName = string.Format("{0};{1};{2};{3};{4};{5}", logon, true, username.ToLower(), group, "", true);
+            //FormsAuthentication.SetAuthCookie(idName, true);
             return View();
         }
     }
